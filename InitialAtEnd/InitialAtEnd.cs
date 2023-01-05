0aacb4581bdfa63b37544d8b57049402bc0684b2
Aleyna Yıldız <aleyyldz9@gmail.com>
2023-01-05 15:22:56 +0300

    initial letter to end added

---
+++ InitialAtEnd/InitialAtEnd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialAtEnd
+{
+    internal class InitialAtEnd
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir kelime giriniz: ");
+            string text = Console.ReadLine();
+            Console.WriteLine(putInitialAtEnd(text));
+        }
+
+        static string putInitialAtEnd(string text)
+        {
+            int length = text.Length;
+            //string newText = text.Insert(length + 1, text.Substring(1));
+            string newText = text.Substring(1) + text[0];
+            return newText;
+        }
+    }
+}
