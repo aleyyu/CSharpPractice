4108e9a8c7897bed23d030d87c823659f945a213
Aleyna Yıldız <aleyyldz9@gmail.com>
2023-01-05 14:35:25 +0300

    Proje dosyası ekle.

---
+++ DrawTriangle/DrawTriangle.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawTriangle
+{
+    internal class DrawTriangle
+    {
+        static void Main(string[] args)
+        {
+            int size;
+            Console.Write("Enter the size of triangle: ");
+            size = Convert.ToInt32(Console.ReadLine());
+            drawTriangle(size);
+        }
+
+        /*
+         
+        size = 7
+         
+        i           yıldız (i+1)    boşluk
+        0-------*          1          7
+        1------* *         2          6
+        2-----* * *        3          5
+        3----* * * *       4          4
+        4---* * * * *      5          3
+        5--* * * * * *     6          2
+        6-* * * * * * *    7          1
+        7* * * * * * * *   8          0
+
+        size = 3 
+        i          (i+1)    
+        0   *       1       3
+        1  * *      2       2
+        2 * * *     3       1
+        3* * * *    4       0
+         */
+
+        static void drawTriangle(int size)
+        {
+            int bosluk = size;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < bosluk; j++)
+                {
+                    Console.Write(" ");
+                }
+                bosluk--;
+                for (int j = 0; j < (i + 1); j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+            /*
+            for (int i = 1; i <= size; i++)
+            {
+                for (int j = 1; j <= size - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+
+            }
+            */
+        }
+    }
+}
---
+++ Fibonacci/Fibonacci.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int depth = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(fibonacciCalculator(depth));
+            int[] fibo = fibonacciCalculator(depth);
+            Console.Write("Fibonacci serisinin sayılarının ortalaması: " + fibonacciAverageCalc(fibo));
+
+        }
+
+        static int[] fibonacciCalculator(int depth)
+        {
+            int[] fibonacci = new int[depth];
+            int sum = 0;
+            double average = 0;
+
+            for (int i = 0; i < depth; i++)
+            {
+                if (i == 0 || i == 1)
+                {
+                    fibonacci[i] = 1;
+                }
+                else
+                {
+                    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+                }
+                sum += fibonacci[i];
+            }
+            return fibonacci;
+        }
+
+        static double fibonacciAverageCalc(int[] fibonacci)
+        {
+            int sum = 0;
+            double average = 0;
+            int depth = fibonacci.Length;
+
+            for (int i = 0; i < depth; i++)
+            {
+                sum += fibonacci[i];
+            }
+            average = (double)sum / depth;
+            return average;
+        }
+    }
+}
