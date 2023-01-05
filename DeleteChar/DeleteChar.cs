e36f31249c53baeb70d050ed4d63db580841859a
Aleyna Yıldız <aleyyldz9@gmail.com>
2023-01-05 15:02:33 +0300

    deletechar gönderildi

---
+++ DeleteChar/DeleteChar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteChar
+{
+    internal class DeleteChar
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Arasında virgülle bir text ve bir index giriniz: ");
+            string input = Console.ReadLine();
+            string[] splittedInput = input.Split(", ");
+            string text = splittedInput[0];
+            int index = Convert.ToInt32(splittedInput[1]);
+            Console.WriteLine(deleteChar(text, index));
+
+        }
+
+        static string deleteChar(string text, int index)
+        { 
+            return text.Remove(index, 1);
+        }
+    }
+}
---
+++ DrawCircle/DrawCircle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawCircle
+{
+    internal class DrawCircle
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        /*
+         radius = 2
+
+           **
+          ****
+         ******
+         
+         */
+
+        static void drawCircle(int radius)
+        {
+
+        }
+    }
+}
